ddb69271cee5b25810800289f793982365102fc6
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2015-02-22 14:09:39 -0800

    initial commit - some data loading functions

---
+++ run_analysis.R
@@ -0,0 +1,25 @@
+library(data.table)
+
+## creates datatables as global variables for test and train datasets
+readUCIFolder = function(UCIFolder) {
+  test_subject <<- read.table(paste(".", UCIFolder, "test/subject_test.txt", sep = "/"))
+  test_measurements <<- read.table(paste(".", UCIFolder, "test/X_test.txt", sep = "/"))
+  test_activity <<-read.table(paste(".", UCIFolder, "test/Y_test.txt", sep = "/"))
+  train_subject <<- read.table(paste(".", UCIFolder, "train/subject_train.txt", sep = "/"))
+  train_measurements <<- read.table(paste(".", UCIFolder, "train/X_train.txt", sep = "/"))
+  train_activity <<- read.table(paste(".", UCIFolder, "train/Y_train.txt", sep = "/"))
+  features <<- read.table(paste(".", UCIFolder, "features.txt", sep = "/"))
+  colNames <<- c("subject",  "activity", as.character(features[,2]))
+}
+
+combineDataColumns = function() {
+  test_combined <<- cbind(test_subject, test_activity, test_measurements)
+  train_combined <<- cbind(train_subject, train_activity, train_measurements)
+}
+
+renameColumnNames = function(dataFrame, columnNames) {
+  numCols = length(columnNames)
+  for(i in 1:numCols) {
+    names(dataFrame)[i] = columnNames[i]
+  }
+}
